d85d4decb33be023ec55fd0c2338d1ff846628a2
Manvi <manvisekhri08@gmail.com>
2019-12-12 16:30:26 +0530

    My first project

---
+++ mean-project/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,23 @@
+import { Component , EventEmitter , Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+// import { Post } from '../post.model';
+import { PostService } from '../post.service';
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+enteredContent = '';
+enteredTitle = '';
+// postCreated = new EventEmitter<Post>();
+constructor( public postsService: PostService) {}
+  // tslint:disable-next-line: align
+  onAddPost(form: NgForm) {
+  if (form.invalid) {
+     return;
+    }
+  this.postsService.addPost(form.value.title, form.value.content);
+  form.resetForm();
+}
+}
