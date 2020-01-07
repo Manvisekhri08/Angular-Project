458f048e080df6a49b8e00e1c18327733ce06045
Manvisekhri08 <44222251+Manvisekhri08@users.noreply.github.com>
2020-01-07 22:55:49 +0530

    Authentication
    Handling User Input

--- mean-project/src/app/auth/login/login.component.ts
+++ mean-project/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
 templateUrl: './login.component.html',
@@ -7,4 +8,7 @@ styleUrls: ['./login.component.css']
 
 export class LoginComponent {
 isLoading = false;
+onLogin(form: NgForm) {
+console.log(form.value);
+}
 }
