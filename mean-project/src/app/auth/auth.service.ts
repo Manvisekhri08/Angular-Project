93127af26cc15891eb54d614707c9447d9e9db52
Manvisekhri08 <manvisekhri8@gmail.com>
2020-01-15 18:01:29 +0530

    Authentication
    Improving UI header

--- mean-project/src/app/auth/auth.service.ts
+++ mean-project/src/app/auth/auth.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private token: string;
-
+  private authStatusListener = new Subject<boolean>();
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
   }
 
   createUser(email: string, password: string) {
@@ -28,6 +33,7 @@ login(email: string, password: string) {
  .subscribe(response => {
 const token = response.token;
 this.token = token;
+this.authStatusListener.next(true);
 });
 }
 }
