09bcd09f26124b81cc9dae8c82ddf2bdf328f682
Manvisekhri08 <manvisekhri08@gmail.com>
2020-01-26 15:34:40 +0530

    Deploying
    Using the Integrated Project

--- mean-project/backend/app.js
+++ mean-project/backend/app.js
@@ -21,9 +21,10 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("images")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/", express.static(path.join(__dirname, "angular")));
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -34,9 +35,12 @@ app.use((req, res, next) => {
     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
-});
+}); */
 
 app.use("/api/posts",postsRoutes);
 app.use("/api/user",userRoutes);
+app.use((rq, res, next) => {
+  res.sendFile(path.join(__dirname, "angular", "index.html"));
+});
 
 module.exports = app;
